693c95f5e93339bd5c30d85fb4af09570a9ec9ff
Jasper Chen <jasper.jiapeng.chen@gmail.com>
2026-07-08 11:47:01 -0700

    update requirements

---
+++ sync_from_sherlock.sh
@@ -0,0 +1,2 @@
+DIFFICE_PATH="/oak/stanford/groups/cyaolai/JasperChen/Software/DIFFICE_jax"
+rsync -avz sherlock-dtn:$DIFFICE_PATH/examples/synthetic_data /Users/jiapchen/Software/DIFFICE_jax/examples/ 
